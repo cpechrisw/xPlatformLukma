df9265c78291086420929424404b9ffd48df5409
cpe.chrisw <cpe.chrisw@gmail.com>
2025-04-09 12:20:44 -0400

    updated file cleanup
    Updated file cleanup

--- FolderCleanup.cs
+++ FolderCleanup.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
 
 namespace xPlatformLukma
 {
@@ -38,10 +40,11 @@ namespace xPlatformLukma
             if (Directory.Exists(_baseFolder))
             {
                 List<string> fileList = SearchFoldersForFiles(_baseFolder, _dayCount, _currentDate);
-
+                
                 if (fileList.Count > 0)
                 {
                     sErrors = DeleteOldFiles(fileList);
+                    sErrors += DeleteEmptyFolders(_baseFolder);
                 }
             }
             else 
@@ -61,8 +64,10 @@ namespace xPlatformLukma
                 string noCaseFileName = f.ToLowerInvariant();
                 if (noCaseFileName.Contains("mp4"))
                 {
-                    //Not sure if GetLastWriteTime is going to work
-                    DateTime fileCreatedDate = File.GetLastAccessTime(f);
+                    //For debugging only
+                    //DateTime fileCreatedDate = System.IO.File.GetLastWriteTime(f);
+                    //Use GetLastAccessTime for actual program
+                    DateTime fileCreatedDate = System.IO.File.GetLastAccessTime(f);
 
                     System.TimeSpan diff = currentDate - fileCreatedDate;
                     if (diff.Days > days)
@@ -81,16 +86,44 @@ namespace xPlatformLukma
             {
                 try
                 {
-                    File.Delete(f);
+                    System.IO.File.Delete(f);
                 }
                 catch (Exception e)
                 {
-                    sErrors += "File: "+ f + " Error: " + e.Message + Environment.NewLine;
+                    sErrors += "Could not delete: "+ f + " Error: " + e.Message + Environment.NewLine;
                 }
             }
             return sErrors;
         }
+        
+        private static string DeleteEmptyFolders(string startLocation)
+        {
+            string sErrors = "";
+            if (!Directory.Exists(startLocation))
+            {
+                return sErrors;
+            }
+           
+            var allDirs = Directory.GetDirectories(startLocation, "*", SearchOption.AllDirectories)
+                       .OrderByDescending(d => d.Count(c => c == Path.DirectorySeparatorChar));
 
+            foreach (string dir in allDirs)
+            {
+                try
+                {
+                    // Only delete if the directory is completely empty (no files or subdirs)
+                    if (Directory.GetFiles(dir).Length == 0 && Directory.GetDirectories(dir).Length == 0)
+                    {
+                        Directory.Delete(dir);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    sErrors += "Could not delete: " + dir + " Error: " + ex.Message + Environment.NewLine;
+                }
+            }
+            return sErrors;
+        }
 
     }
 }
